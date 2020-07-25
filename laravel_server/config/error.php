a1b1b40c45c1217f367aeb2d136d6b6f6bc82047
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-07-26 08:46:10 +0900

    future/user

---
+++ laravel_server/config/error.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+	'ERROR_MASTER_DATA_UPDATE' => 1,
+    'ERROR_DB_UPDATE' => 2,
+);
--- laravel_server/routes/web.php
+++ laravel_server/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/registration', 'RegistrationController@Registration');
