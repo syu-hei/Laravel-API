2c962c09d87a30c78ebcd02693c6079ea30bfbee
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-11-28 05:48:55 +0900

    Update PresentController.php

--- Laravel:Server/app/Http/Controllers/PresentController.php
+++ Laravel:Server/app/Http/Controllers/PresentController.php
@@ -13,22 +13,18 @@ class PresentController extends Controller
 		$client_master_version = $request->client_master_version;
 		$user_id = $request->user_id;
 
-		//マスターデータチェック
 		if (!MasterDataService::CheckMasterDataVersion($client_master_version)) {
 			return config('error.ERROR_MASTER_DATA_UPDATE');
 		}
 
-		//user_profileテーブルからレコードを取得
 		$user_profile = UserProfile::where('user_id', $user_id)->first();
-		//レコード存在チェック
+
 		if (!$user_profile) {
 			return config('error.ERROR_INVALID_DATA');
 		}
 
-		//user_presentテーブルからレコードを取得
 		$user_present_list = UserPresent::where('user_id', $user_id)->get();
 
-		//クライアントへのレスポンス
 		$response = array(
 			'user_present' => $user_present_list,
 		);
@@ -41,31 +37,24 @@ class PresentController extends Controller
 		$user_id = $request->user_id;
 		$present_id = $request->present_id;
 
-		//マスターデータチェック
 		if (!MasterDataService::CheckMasterDataVersion($client_master_version)) {
 			return config('error.ERROR_MASTER_DATA_UPDATE');
 		}
 
-		//user_profileテーブルのレコードを取得
 		$user_profile = UserProfile::where('user_id', $user_id)->first();
-		//レコード存在チェック
 		if (!$user_profile) {
 			return config('error.ERROR_INVALID_DATA');
 		}
+        $user_present = UserPresent::where('user_id', $user_id)->where('present_id', $present_id)->first();
 
-		//user_presentテーブルからレコードを取得
-		$user_present = UserPresent::where('user_id', $user_id)->where('present_id', $present_id)->first();
-		//レコードが存在するかチェック
 		if (!$user_present) {
 			return config('error.ERROR_INVALID_DATA');
 		}
 
-		//受け取り期限チェック
 		if (strtotime($user_present->limited_at) < time()) {
 			return config('error.ERROR_INVALID_SCHEDULE');
 		}
 
-		//プレゼントからアイテム付与
 		switch ($user_present->item_type) {
 			case config('constants.ITEM_TYPE_CRYSTAL'):
 				$user_profile->crystal += $user_present->item_count;
@@ -80,7 +69,6 @@ class PresentController extends Controller
 				break;
 		}
 
-		//データの書き込み
 		try {
 			$user_profile->save();
 			$user_present->delete();
@@ -88,7 +76,6 @@ class PresentController extends Controller
 			return config('error.ERROR_DB_UPDATE');
 		}
 
-		//クライアントへのレスポンス
 		$user_profile = UserProfile::where('user_id', $user_id)->first();
 		$user_present_list = UserPresent::where('user_id', $user_id)->get();
 		$response = array(
