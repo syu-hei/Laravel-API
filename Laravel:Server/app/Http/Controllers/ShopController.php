8b8e17ab5f2d3f46683bbb347d0c451c85284b6a
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-11-28 05:51:57 +0900

    Update ShopController.php

--- Laravel:Server/app/Http/Controllers/ShopController.php
+++ Laravel:Server/app/Http/Controllers/ShopController.php
@@ -11,29 +11,24 @@ class ShopController extends Controller
 		$user_id = $request->user_id;
 		$shop_id = $request->shop_id; //item_120
 
-		//user_profileテーブルのレコードを取得
 		$user_profile = UserProfile::where('user_id', $user_id)->first();
-		//レコード存在チェック
 		if (!$user_profile) {
 			return config('error.ERROR_INVALID_DATA');
 		}
 
 		$master_shop = MasterShop::GetMasterShopByShopId($shop_id);
-		//マスターデータ存在チェック
 		if (is_null($master_shop)) {
 			return config('error.ERROR_INVALID_DATA');
 		}
 
 		$user_profile->crystal += $master_shop->amount;
 
-		//データの書き込み
 		try {
 			$user_profile->save();
 		} catch (\PDOException $e) {
 			return config('error.ERROR_DB_UPDATE');
 		}
 
-		//クライアントへのレスポンス
 		$user_profile = UserProfile::where('user_id', $user_id)->first();
 		$response = array(
 			'user_profile' => $user_profile,
