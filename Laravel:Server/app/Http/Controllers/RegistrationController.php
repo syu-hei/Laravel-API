24280c63b6133d3fff90fe58393c3d1af9c9ca8f
syu-hei <53079859+syu-hei@users.noreply.github.com>
2020-11-28 05:51:22 +0900

    Update RegistrationController.php

--- Laravel:Server/app/Http/Controllers/RegistrationController.php
+++ Laravel:Server/app/Http/Controllers/RegistrationController.php
@@ -9,10 +9,8 @@ class RegistrationController extends Controller
 {
 	public function Registration(Request $request)
 	{
-		//ユーザーIDの決定
-		$user_id = uniqid(); //例:4b3403665fea6
+		$user_id = uniqid();
 
-		//初期データの設定
 		$user_profile = new UserProfile;
 		$user_profile->user_id = $user_id;
 		$user_profile->user_name = 'user name';
@@ -21,14 +19,12 @@ class RegistrationController extends Controller
 		$user_profile->friend_coin = config('constants.FRIEND_COIN_DEFAULT');
 		$user_profile->tutorial_progress = config('constants.TUTORIAL_START');
 
-		//データの書き込み
 		try {
 			$user_profile->save();
 		} catch (\PDOException $e) {
 			return config('error.ERROR_DB_UPDATE');
 		}
 
-		//クライアントへのレスポンス
 		$user_profile = UserProfile::where('user_id', $user_id)->first();
 
 		$response = array('user_profile' => $user_profile);
